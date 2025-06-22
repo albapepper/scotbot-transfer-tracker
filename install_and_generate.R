57f06664240ee5cde8f9c211b98a896316e812ab
albapepper <albapepper@gmail.com>
2025-06-21 22:18:41 -0400

    Update install_and_generate.R

--- install_and_generate.R
+++ install_and_generate.R
@@ -1,11 +1,20 @@
-install.packages("remotes", repos = "http://cran.us.r-project.org")
-remotes::install_github("JaseZiv/worldfootballR")
-install.packages(c("dplyr", "readr", "purrr"), repos = "http://cran.us.r-project.org")
+# Set a custom library path
+lib_path <- Sys.getenv("R_LIBS_USER")
+if (!dir.exists(lib_path)) dir.create(lib_path, recursive = TRUE)
 
-library(worldfootballR)
-library(dplyr)
-library(readr)
+# Install remotes and use it to install worldfootballR
+install.packages("remotes", repos = "http://cran.us.r-project.org", lib = lib_path)
+remotes::install_github("JaseZiv/worldfootballR", lib = lib_path)
 
+# Install other dependencies
+install.packages(c("dplyr", "readr", "purrr"), repos = "http://cran.us.r-project.org", lib = lib_path)
+
+# Load libraries from the custom path
+library(worldfootballR, lib.loc = lib_path)
+library(dplyr, lib.loc = lib_path)
+library(readr, lib.loc = lib_path)
+
+# Generate player list
 leagues <- c("ENG-Premier League", "ESP-La Liga", "GER-Bundesliga", "ITA-Serie A", "FRA-Ligue 1")
 season <- 2024
 

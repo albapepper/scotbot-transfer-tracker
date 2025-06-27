ba6063c2fd102ae9e76f5ba4057edf884d57eb9c
albapepper <albapepper@gmail.com>
2025-06-27 15:33:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import re
 app = Flask(__name__)
 
 API_KEY = "df28d02564c64ca891c9e91da26e32fa"
-ENDPOINT = "https://newsapi.org/v2/everything"
+ENDPOINT = "https://newsapi.org/v2/everything?q=sports&apiKey=df28d02564c64ca891c9e91da26e32fa"
+
 
 try:
     with open("player_names.txt", "r", encoding="utf-8") as f:
